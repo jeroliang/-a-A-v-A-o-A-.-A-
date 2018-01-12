897fc54ab22b064005ce1f63e85d909e0c4c6a40
jero <jeroliang@gmail.com>
2018-01-12 11:53:52 +0800

    corrdinator layout demo

--- app/src/main/java/demo/jero/layout/CoordinatorLayActivity.java
+++ app/src/main/java/demo/jero/layout/CoordinatorLayActivity.java
@@ -1,10 +1,10 @@
-package demo.jero;
+package demo.jero.layout;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,15 +12,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class MainActivity extends AppCompatActivity
+import demo.jero.R;
+
+public class CoordinatorLayActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_coordinator_lay);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -56,7 +57,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.coordinator_lay, menu);
         return true;
     }
 
@@ -81,13 +82,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.a_demo) {
-            startActivity(new Intent(MainActivity.this, ADemoActivity.class));
-        } else if (id == R.id.lay_demo) {
-            startActivity(new Intent(MainActivity.this, LayDemoActivity.class));
-        } else if (id == R.id.nav_slideshow) {
+        if (id == R.id.nav_1) {
+            // Handle the camera action
+        } else if (id == R.id.nav_2) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_3) {
+
+        } else if (id == R.id.nav_4) {
 
         } else if (id == R.id.nav_share) {
 
